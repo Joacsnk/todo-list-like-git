594b39ce865745d9c0959590b1518c2aafaa1cdc
Jackson Lobo <Joacsnk@gmail.com>
2026-01-29 16:08:40 -0300

    Refatoração
    Refatoraçaõ de repetição
    descrição mais explicativa

--- logic.py
+++ logic.py
@@ -4,38 +4,50 @@ import typer
 
 class Json():
     
+    
     def __init__(self) -> None:
-        self.file = "tasks.json" 
+        self.file = "tasks.json" # Defini o nome do arquivo json 
     
     
-    def check_Json(self): # verificação do json
+    # Ler o arquivo
+    def ler_Json(self): 
+        with open(self.file, "r", encoding="utf-8") as f:
+            return json.load(f) 
+            
+            
+    # Escrever o arquivo
+    def escrever_Json(self, tarefas):
+        with open(self.file, "w", encoding="utf-8") as f:
+            json.dump(tarefas, f, indent=2, ensure_ascii=False)
+       
+       
+    # Checkar se Json existe ou não tem um []
+    def check_Json(self): 
         if not os.path.exists(self.file) or os.path.getsize(self.file) == 0: # caso não exista ou esteja sem o []
             with open (self.file, "w", encoding="utf-8") as f: # cria um json novo com []
                 json.dump([], f, indent=2)
                 
+    
+    # Adicinar tarefa
+    def add_Task(self, task: dict): 
+        tarefas = self.ler_Json()
         
-    def add_Task(self, task: dict): # adiciona a tarefa no json
-        with open(self.file, "r", encoding="utf-8") as f: # carrega o json
-            tarefas = json.load(f)
+        tarefas.append(task) 
         
-        
-        
-        tarefas.append(task) # adiciona
-        
-        with open(self.file, "w", encoding="utf-8") as f: # escreve essa adição e salva
-            json.dump(tarefas, f, indent=2, ensure_ascii=False)
+        self.escrever_Json(tarefas)
+           
             
+    # Lista as tarefas no json        
+    def list_Tasks(self): 
+        tarefas = self.ler_Json()
             
-    def list_Tasks(self): # lista as tarefas no json
-        with open(self.file, "r", encoding="utf-8") as f: # carrega o json
-            tarefas = json.load(f)
-            
-        if len(tarefas) == 0: # verificar se existe tarefas
+        if len(tarefas) == 0: # Verificar se existe tarefas
             Error().existence_E(1)
 
-        for index, tarefa in enumerate(tarefas, start=1): # lista as tarefas no loop 
 
-            if tarefa["done"]: # caso feito, ele muda a visualização
+        for index, tarefa in enumerate(tarefas, start=1): # Lista as tarefas no loop 
+
+            if tarefa["done"]: # Caso feito, ele muda a visualização
                 status = "✔"
             else:
                 status = " "
@@ -45,25 +57,28 @@ class Json():
             print(f"{index}. [{status}] {titulo}") # visualização
   
   
+    # Verificação da existência de tarefa
     def task_Exists(self, titulo):
-        with open(self.file, "r", encoding="utf-8") as f:
-            tarefas = json.load(f)
+        tarefas = self.ler_Json()
         
         for i in tarefas:
             if titulo == i["titulo_tarefa"]:
                 return True
         return False
     
+    
+    # Deifinir tarefa como "done"
     def done_Task(self, task):
-        with open(self.file, "r", encoding="utf-8") as f:
-            tarefas = json.load(f)
+        tarefas = self.ler_Json()
             
         for tarefa in tarefas:
             if tarefa["titulo_tarefa"] == task:
-                tarefa["done"] = True
+                if tarefa["done"] == True:
+                    Error().done_E(1)
+                else:
+                    tarefa["done"] = True
                 
-        with open(self.file, "w", encoding="utf-8") as f:
-            json.dump(tarefas, f, indent=4, ensure_ascii=False)
+        self.escrever_Json(tarefas)
 
 class Formatacao():
     
@@ -71,7 +86,8 @@ class Formatacao():
         pass
     
     
-    def format_Task(self, task, desc): # formatação da tarefa
+    # Formatação da tarefa
+    def format_Task(self, task, desc): 
         tarefa = {
             "titulo_tarefa": task, # titulo
             "descricao": desc, # descrição
@@ -79,40 +95,52 @@ class Formatacao():
         }
         return tarefa
     
-    
+    # Limpar tela
     def clear(self):
         os.system("cls")
         
 
 class Error():
     
+    
     def __init__(self) -> None:
         pass
     
-    
+    # Parâmetros
     def argumment_E(self, n_error):
         match n_error:
-            case 1:
+            case 1: # Título vazio
                 typer.echo("\033[31mErro argumment_01: Título da task não pode estar vazia.\033[m")
                 raise typer.Exit(code=1)
-            case 2:
+            case 2: # Descrição vazia
                 typer.echo("\033[31mErro argumment_02: Descrição não pode ser vazia.\033[m")
                 raise typer.Exit(code=1)
     
+    
+    # Duplicidade
     def duplicate_E(self, n_error):
         match n_error:
-            case 1:
+            case 1: # Tarefa duplicada
                 typer.echo("\033[31mErro duplicate_01: Tarefa já existente.\033[m")
                 raise typer.Exit(code=1)
                 
 
+    # Existência
     def existence_E(self, n_error):
         match n_error:
-            case 1:
+            case 1: # Não existe tarefas
                 typer.echo("\033[31mErro existence_01: Não existe nenhuma tarefa.\033[m")
                 raise typer.Exit(code=1)
-            case 2:
+            case 2: # Não existe nenhuma tarefa com esse título
                 typer.echo("\033[31mErro existence_02: Não existe tarefa com esse título.\033[m")
+                raise typer.Exit(code=1)
+    
+    
+    # Feito
+    def done_E(self, n_error):
+        match n_error:
+            case 1: # Já finalizada
+                typer.echo("\033[31mErro done_01: Tarefa já finalizada.\033[m")
                 raise typer.Exit(code=1)
                 
                 
--- todo.py
+++ todo.py
@@ -1,50 +1,57 @@
-import typer # edição de CLI
+import typer # Edição de CLI
 from logic import Json as j, Formatacao as f, Error as e
-
-
-def main():
-    j().check_Json() # verifica se o json existe ou sem o []
-    app()
 
 app = typer.Typer()
 
+# Def de inicialização
+def main():
+    j().check_Json() # Verifica se o json não existe ou está sem o []
+    app()
 
+
+# Adição de uma nova tarefa. Necessário o título e sua descrição
 @app.command()
 def add(
     task: str = typer.Argument(None),
     desc: str = typer.Argument(None),
-): # adiciona uma tarefa (task: titulo, desc: descriçao)
+):
     
-# tratamento de erro:
-    if task is None:
+# Tratamento de erro:
+    if task is None: # Sem título
         e().argumment_E(1)
-    if desc is None:
+    if desc is None: # Sem descrição
         e().argumment_E(2)
-    if j().task_Exists(task):
+    if j().task_Exists(task): # Task com mesmo título
         e().duplicate_E(1)
     
-    # criação da tarefa
-    tarefa_dict = f().format_Task(task, desc) # formatação
+    # Criação da tarefa
+    tarefa_dict = f().format_Task(task, desc) # Formatação para um dict
     j().add_Task(tarefa_dict) # adiciona esse dict no json
     
     typer.echo(f"\033[32mTarefa adicionada: {task}\033[m")
 
+
+# Listar todas as tarefas e se estão feitas
 @app.command()
-def list(): # lista as tarefas
+def list(): 
     j().list_Tasks()
 
+
+# Marcar a tarefa como feita
 @app.command()
-def done(task: str = typer.Argument(None)): # marca como feito a tarefa
-    if task is None:
+def done(
+    task: str = typer.Argument(None)
+    ): 
+    # Tratamento de erros
+    if task is None: # Sem o argumento principal
         e().argumment_E(1)
-    if not j().task_Exists(task):
+    if not j().task_Exists(task): # Task não existe
         e().existence_E(2)
     
-    j().done_Task(task)
+    j().done_Task(task) # Marca como done
     
     typer.echo(f"\033[32mTarefa realizada: {task}\033[m")
     
-
 
 if __name__ == "__main__":
     f().clear()
